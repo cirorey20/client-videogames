c4b402b664029541a5cd3656f5aa16f0433dacdc
cirorey20 <cirorey95@gmail.com>
2022-05-07 18:37:52 -0300

    add a bootstrap card

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -22,16 +22,21 @@ const Details = (props) => {
                         
                     :
                     (
-                        <div className='containerDetails'>
-                            <div className='detailsData'>
-                                <h1>{details.name}</h1>
-                                <div className='description'>{details.description}</div> <br />
-                                <div><strong>Released:</strong> {details.released}</div>
-                                <div><strong>Rating:</strong> {details.rating}</div>
-                                <div><strong>Platforms:</strong> {details.platforms}</div>
-                            </div>
-                            <div className='detailsImg'>
-                                <img src={details.img} alt="NOT_FOUND" />
+                        <div>
+                            <h1 className='text-muted text-center mb-4'>{details.name}</h1>
+                            <br />
+                            <div className=' containerDetails'>
+                                <div className='col'>
+                                <div className="card mb-3">
+                                    <img className='card-img-top' src={details.img} alt="NOT_FOUND" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Rating: {details.rating}</h5>
+                                        <p className="card-text">{details.description}</p>
+                                        <p className="card-text">Platforms: <small className="text-muted">{details.platforms}</small></p>
+                                        <p className="card-text">Released: <small className="text-muted">{details.released}</small></p>
+                                    </div>
+                                </div>
+                                </div>
                             </div>
                         </div>
                     )
